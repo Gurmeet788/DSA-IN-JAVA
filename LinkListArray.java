e9511472dd663f820d546bb999c0c7272129d3af
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-01-04 13:27:15 +0500

    Add insert kth value and node function

--- LinkListArray.java
+++ LinkListArray.java
@@ -1,10 +1,8 @@
 public class LinkListArray {
     public static void main(String[] args) {
         int array[] = {1,2,8,7,9};
-
         Node head = covertArr(array);
-        //printFull(head);
-        head = insertLast(head, 10);
+        head = insertKthvalue(head, 55, 8);
         printFull(head);
     }
 
@@ -158,8 +156,13 @@ public class LinkListArray {
         return newNode;
     }
 
-    public static Node insertLast(Node head, int value){
+    public static Node insertLast(Node head, int value){  // Insert at last
         Node newNode = new Node(value);
+
+        if (head == null) {
+
+            return head = newNode;
+        }
         Node temp = head;
         
         while (temp.gettNext() != null) {
@@ -167,6 +170,67 @@ public class LinkListArray {
             temp = temp.gettNext();            
         }
         temp.SetNext(newNode);
+        return head;
+    }
+
+    public static Node insertKthNode(Node head, int value, int k){ // insert at kth postion
+
+        if (k == 1) {
+
+            return insertHead(head, value);
+        }
+        if (head == null && k > 1) {
+
+            System.out.println("Not possiable");
+
+            return null;
+        }
+
+        Node newNode = new Node(value);
+        Node curr = head;
+
+        for(int i = 1; i < k; i++){
+
+            if (i == k - 1 ) {
+
+                newNode.SetNext(curr.gettNext());
+
+                curr.SetNext(newNode);
+            }
+            curr = curr.gettNext();
+        }
+        return head;
+    }
+
+    public static Node insertKthvalue(Node head, int value, int k){ //Insert at kth value
+
+        if (head == null || head.getData() == k) {
+
+            return insertHead(head, value);
+        }
+
+        Node newNode = new Node(value);
+        Node curr = head;
+        boolean flage = false;
+        while (curr.gettNext() != null) {
+            
+            if (curr.gettNext().getData() == k) {
+                
+                newNode.SetNext(curr.gettNext());
+
+                curr.SetNext(newNode);
+
+                flage = true;
+                break;
+            }
+            curr = curr.gettNext();
+        }
+
+        if (flage == false) {
+
+            System.out.println("Value not found");
+        }
+
         return head;
     }
 }
